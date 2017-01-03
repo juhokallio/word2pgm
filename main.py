e14e67e0de27c89b375fd795120821c4fca6d5e1
Juho Kallio <juho.kallio@futurice.com>
2017-01-03 06:28:57 +0200

    Refactor tests to use only Word2pgm

--- main.py
+++ main.py
@@ -16,32 +16,37 @@ class Word2pgm:
         self.parser = FinnishParser()
         self.lstm_model = AnnModel(base_vector_size + grammar_vector_size, look_back, lstm_layer_size, lstm_layers)
 
-    def train(self, text, lstm_epochs, lstm_batch_size, word2vec_iterations=1000):
+    def train(self, text, lstm_epochs, lstm_batch_size, word2vec_iterations=1000, test_data_portion=0.2):
         parsed_words, sentence_start_indexes = self.parser.parse(text)
         print("words parsed")
         self.text_model = TextModel(parsed_words, sentence_start_indexes, base_size=self.base_vector_size, grammar_size=self.grammar_vector_size, word2vec_iterations=word2vec_iterations)
         self.cosine_similarity_pdf = self.text_model.get_cosine_similarity_pdf()
         vector_data = [self.text_model.word_to_vector(w) for w in parsed_words]
-        split_index = int(len(vector_data) * 0.2)
+        split_index = int(len(vector_data) * test_data_portion)
         training_data = vector_data[split_index:]
         test_data = vector_data[:split_index]
+        print("training data length: {}".format(len(training_data)))
+        print("test data length: {}".format(len(test_data)))
         self.lstm_model.train(training_data, [], epochs=lstm_epochs, batch_size=lstm_batch_size)
 
-    def predict_text(self, words_to_predict, history=[], text=[]):
+    def predict_text(self, words_to_predict, history=[]):
         if words_to_predict > 0:
-            vector = self.ann_model.predict(history)
+            vector = self.lstm_model.predict(history)
             word = self.text_model.likeliest(vector)
-            history.append(text_model.word_to_vector(word))
-            text.append(word)
-            return self.predict_text(words_to_predict-1, history, text)
+            history.append(self.text_model.word_to_vector(word))
+            return [word] + self.predict_text(words_to_predict-1, history)
         else:
-            return text
+            return []
 
     def print_cosine_similarity_densities(self):
         print("cosine similarity densities (-1, 0, 1):")
         print(self.cosine_similarity_pdf(-1))
         print(self.cosine_similarity_pdf(0))
         print(self.cosine_similarity_pdf(1))
+
+    def text_to_vectors(self, text):
+        parsed_words, _ = self.parser.parse(text)
+        return [self.text_model.word_to_vector(w) for w in parsed_words]
 
 
 def read_file(file_name):
@@ -123,17 +128,17 @@ if __name__ == "__main__":
 class Word2pgmTest(unittest.TestCase):
 
     default_settings = {
-            "base_vector_size": 10,
-            "grammar_vector_size": 10,
-            "look_back": 4,
-            "lstm_layer_size": 20,
+            "base_vector_size": 7,
+            "grammar_vector_size": 5,
+            "look_back": 2,
+            "lstm_layer_size": 100,
             "lstm_layers": 2
             }
 
     def test_probability_distributions(self):
         word2pgm = Word2pgm(**self.default_settings)
         text = read_file("data/finnish/pg45271.txt")[:10000]
-        word2pgm.train(text, lstm_epochs=1, lstm_batch_size=100, word2vec_iterations=1000)
+        word2pgm.train(text, lstm_epochs=1, lstm_batch_size=100, word2vec_iterations=1000, test_data_portion=0.5)
         self.assertTrue(word2pgm.cosine_similarity_pdf(-1) < word2pgm.cosine_similarity_pdf(0),
                 msg="cosine similarity density in -1 was higher than in 0")
         self.assertTrue(word2pgm.cosine_similarity_pdf(1) < word2pgm.cosine_similarity_pdf(0),
@@ -143,28 +148,14 @@ class Word2pgmTest(unittest.TestCase):
                 msg="cosine similarity density in -1 was not the same as in 1")
 
     def test_predicting_with_tiny_input(self):
-        look_back = 2
-        base_vector_size = 5
-        grammar_vector_size = 3
-        lstm_neurons = 100
-        lstm_count = 2
-
+        word2pgm = Word2pgm(**self.default_settings)
         text = "Koiraa alkaa ärsyttämään ohjelmointi. Se ei enää jaksa."
-        parser = FinnishParser()
-        parsed_words, sentence_start_indexes = parser.parse(text)
-        text_model = TextModel(parsed_words, sentence_start_indexes, base_size=base_vector_size, grammar_size=grammar_vector_size, word2vec_iterations=1)
-        lstm_model = AnnModel(base_vector_size + grammar_vector_size, look_back, lstm_neurons, lstm_count)
-        training_vectors = [text_model.word_to_vector(w) for w in parsed_words]
-        lstm_model.train(training_vectors, [], epochs=500, batch_size=3000)
+        word2pgm.train(text, lstm_epochs=50, lstm_batch_size=300, word2vec_iterations=1, test_data_portion=0.0)
 
-        history = [
-                text_model.word_to_vector(parser.analyse("Koiraa")),
-                text_model.word_to_vector(parser.analyse("alkaa")),
-                text_model.word_to_vector(parser.analyse("ärsyttämään"))
-                ]
-        predicted = lstm_model.predict(history)
-        self.assertEqual(text_model.likeliest(predicted).base, "ohjelmointi")
+        history = word2pgm.text_to_vectors("Koiraa alkaa ärsyttämään")
+        predicted = word2pgm.predict_text(1, history=history)
+        self.assertEqual(predicted[0].base, "ohjelmointi")
 
-        predicted_text = predict_text(text_model, lstm_model, 30)
+        predicted_text = word2pgm.predict_text(30, history=[])
         self.assertEqual(predicted_text[0].base, "koira")
         self.assertEqual(predicted_text[1].base, "alkaa")
