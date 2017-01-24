c8eb8ce7fd41e3d3b48ef16fc169c15b256fe0ba
Juho Kallio <juho.kallio@futurice.com>
2017-01-24 19:01:17 +0200

    Add nsphere cap calculations

--- nsphere.py
+++ nsphere.py
@@ -4,18 +4,24 @@
 from __future__ import division
 import unittest
 import math
-from scipy.special import gamma
+from scipy.special import gamma, betainc
 
 
 def surface(r, n):
-    numerator = 2 * math.pi ** (n * 0.5)
-    denominator = gamma(0.5 * n)
-    return (numerator / denominator) * r ** (n - 1)
+    multiplier = (2 * math.pi ** (n * 0.5)) / gamma(0.5 * n)
+    return multiplier * r ** (n - 1)
     
 def volume(r, n):
     numerator = math.pi ** (n * 0.5)
     denominator = gamma(0.5 * n + 1)
     return (numerator / denominator) * r ** n
+
+def cap(r, n, alpha):
+    if alpha < 0.5 * math.pi:
+        return 0.5 * surface(r, n) * betainc(0.5*(n-1), 0.5, math.sin(alpha)**2)
+    else:
+        non_surface = 0.5 * surface(r, n) * betainc(0.5*(n-1), 0.5, math.sin(math.pi-alpha)**2)
+        return surface(r, n) - non_surface
 
 
 class TestNPhere(unittest.TestCase):
@@ -31,3 +37,11 @@ class TestNPhere(unittest.TestCase):
                 msg="Failed to calculate unit circle surface")
         self.assertAlmostEqual(4*math.pi, surface(1, 3), delta=0.001,
                 msg="Failed to calculate unit ball surface")
+
+    def test_cap_surface(self):
+        self.assertAlmostEqual(0.5 * surface(1, 3), cap(1, 3, 0.5 * math.pi), delta=0.001,
+            msg="Cap with alpha 0.5pi was not half the surface")
+        self.assertAlmostEqual(surface(1, 3), cap(1, 3, math.pi), delta=0.001,
+            msg="Cap with alpha pi was different from surface")
+        self.assertEqual(0, cap(4, 5, 0),
+            msg="Cap with alpha 0 was wrong")
